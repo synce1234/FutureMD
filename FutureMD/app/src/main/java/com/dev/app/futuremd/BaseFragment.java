dede9f10840c4df21734dd1118fc25d2060e25f5
Tran Tinh <trantinh.10101991@gmail.com>
2017-09-17 01:36:32 +0700

    Update BaseFragment class

---
+++ FutureMD/app/src/main/java/com/dev/app/futuremd/BaseFragment.java
@@ -0,0 +1,10 @@
+package com.dev.app.futuremd;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by tinh.tran on 9/17/2017.
+ */
+
+public abstract class BaseFragment extends Fragment{
+}
